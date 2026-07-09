1b22a06550b46ba2262a9f8779060e683d9ab831
jay stanfield <jajen1099.js@gmail.com>
2026-07-09 17:10:54 -0400

    Bump Great Imports to 0.1.3

--- great-imports.php
+++ great-imports.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Great Imports
  * Description: One-time Eventbrite URL importer for collecting event review candidates.
- * Version: 0.1.2
+ * Version: 0.1.3
  * Author: Great Imports
  * Text Domain: great-imports
  */
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'GREAT_IMPORTS_VERSION', '0.1.2' );
+define( 'GREAT_IMPORTS_VERSION', '0.1.3' );
 define( 'GREAT_IMPORTS_FILE', __FILE__ );
 define( 'GREAT_IMPORTS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GREAT_IMPORTS_URL', plugin_dir_url( __FILE__ ) );
