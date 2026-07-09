4fced77397198f389bb056f4c1f1431276222318
jay stanfield <jajen1099.js@gmail.com>
2026-07-09 17:23:17 -0400

    Include evidence records in uninstall cleanup

--- uninstall.php
+++ uninstall.php
@@ -15,17 +15,20 @@ if ( ! $delete_data ) {
     return;
 }
 
-$candidates = get_posts(
-    array(
-        'post_type'      => 'gi_candidate',
-        'post_status'    => 'any',
-        'posts_per_page' => -1,
-        'fields'         => 'ids',
-    )
-);
+foreach ( array( 'gi_candidate', 'gi_evidence' ) as $post_type ) {
+    $posts = get_posts(
+        array(
+            'post_type'      => $post_type,
+            'post_status'    => 'any',
+            'posts_per_page' => -1,
+            'fields'         => 'ids',
+        )
+    );
 
-foreach ( $candidates as $candidate_id ) {
-    wp_delete_post( (int) $candidate_id, true );
+    foreach ( $posts as $post_id ) {
+        wp_delete_post( (int) $post_id, true );
+    }
 }
 
 delete_option( 'great_imports_delete_data_on_uninstall' );
+delete_option( 'great_imports_eventbrite_private_token' );
